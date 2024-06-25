7020fff79ddfe93295ae941aa561b0d36a4c315d
RashmikaDushan <kbrashmikadushan@gmail.com>
2024-06-25 18:17:15 +0530

    reading the value correctly

--- BLE_Client/Client.py
+++ BLE_Client/Client.py
@@ -1,24 +1,55 @@
 import asyncio
 from bleak import BleakScanner,BleakClient
-
-MODEL_NBR_UUID = "c3fd1614-8aec-4c3d-b7b9-b2aafdfbec86"
+from bleak.backends.characteristic import BleakGATTCharacteristic
+from bleak.backends.device import BLEDevice
+from bleak.backends.scanner import AdvertisementData
+import time
 
 async def main():
-    devices = await BleakScanner.discover()
-    for i in range(len(devices)):
-        print(i+1,". ", devices[i])
-    if len(devices) == 0:
-        print("No devices found")
-        return
-    else:
-        device_number = int(input("Enter the number of the device you want to connect to: "))-1
-        device_mac = devices[device_number].address
-        print("Connecting to", devices[device_number].name, "with MAC address", device_mac)
+    try:
+        devices = await BleakScanner.discover()
+        for i in range(len(devices)):
+            print(i+1,". ", devices[i])
 
-        async with BleakClient(device_mac) as client:
-            # print(client.services)
-            model_number = await client.read_gatt_char(MODEL_NBR_UUID)
-            print("Model Number: {0}".format("".join(map(chr, model_number))))
+        if len(devices) == 0:
+            print("No devices found")
+            return
+        else:
+            device_number = int(input("Enter the number of the device you want to connect to: "))-1
+            device = devices[device_number]
+            print("Connecting to", device.name, "with MAC address", device.address)
 
+            async with BleakClient(device.address) as client:
+                try:
+                    client.pair()
+                    services = client.services
+                    print(f"Services for {device.name} ({device.address}):")
+                    service_number = 0
+                    service_uuids = []
+                    for service in services:
+                        print(f"{service_number+1} - {service.uuid}")
+                        service_uuids.append(service.uuid)
+                        service_number += 1
+                    selected_service_number = int(input("Enter the number of the service you want to read: "))-1
+                    selected_service = services.get_service(service_uuids[selected_service_number])
+                    characteristic_number = 0
+                    characteristic_uuids = []
+                    for characteristic in service.characteristics:
+                        print(f"  {characteristic_number+1} - Characteristic: {characteristic.uuid}, Properties: {characteristic.properties}")
+                        characteristic_uuids.append(characteristic.uuid)
+                        characteristic_number += 1
+                    selected_characteristic_number = int(input("Enter the number of the characteristic you want to read: "))-1
+                    characteristic_uuid = characteristic_uuids[selected_characteristic_number]
+                    print(f"Service {selected_service.uuid}")
+                    print(f"Characteristic {characteristic_uuid}")
+                    while True:
+                        value = await client.read_gatt_char(characteristic_uuid)
+                        print("Model Number: {0}".format("".join(map(chr, value))))
+                        time.sleep(0.5)
+                except Exception as e:
+                    print(f"Failed to connect to {device.name} ({device.address}): {e}")
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 asyncio.run(main())
